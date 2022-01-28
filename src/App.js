47c9a9a015a718e75e48314ce183cf784e2d8d1b
MdHasanRaju <mdhasanraju2017@gmail.com>
2022-01-28 21:27:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import 'react-loading-skeleton/dist/skeleton.css'
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import FooterBottom from "./components/FooterBottom";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/products/:id" element={<Product />} />
+      </Routes>
+      <Footer/>
+      <FooterBottom/>
+    </>
   );
 }
 
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='text-center'>
+            Here you can contact use...
+        </div>
+    );
+};
+
+export default Contact;
